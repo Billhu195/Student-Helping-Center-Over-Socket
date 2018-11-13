5a85516baee5dce589f0f971aa8442d2bd106947
hujianhu <billhu@LAPTOP-28H4TAC9.localdomain>
2018-11-13 10:01:39 +0800

    Add test1.c

---
+++ A3/test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "freq_list.h"
+#include "worker.h"
+
+int main() {
+    Node *head = NULL;
+    char **filenames = init_filenames();
+    char *listfile = "./a3-2018/labs/lab1/index";
+    char *namefile = "./a3-2018/labs/lab1/filenames";
+
+    read_list(listfile, namefile, &head, filenames);
+    display_list(head, filenames);
+}
